d696eff9aa0e77a20eac09ad722c11da0606a21e
Nwe Yi Aung <nweyiaung1990@gmail.com>
2021-11-22 08:48:48 +0630

    change some code in my_branch - 2211

---
+++ app/src/main/java/com/developerstar/pos/VoucherNoSetupActivity.java
@@ -0,0 +1,14 @@
+package com.developerstar.pos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VoucherNoSetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voucher_no_setup);
+    }
+}
